1dc125cc89b6bf502c79c41b01767316e28baede
Daniil Ardiukov <danielardukov@gmail.com>
2023-09-25 20:24:18 +0500

    change username

--- database/src/main/java/daniil/ardiukov/UserDao.java
+++ database/src/main/java/daniil/ardiukov/UserDao.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 public class UserDao {
     public Optional<User> findbyId(Long id) {
         User user = new User();
-        user.setName("Андрей");
+        user.setName("Даниил");
         return Optional.of(user);
     }
 }
